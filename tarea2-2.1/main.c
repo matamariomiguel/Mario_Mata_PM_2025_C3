11b51ef861f941b92bce0958c6557def8009aef5
Mario Mata <matamariomiguel@gmail.com>
2025-09-25 21:55:48 -0400

    tarea2 1

---
+++ tarea2-2.1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Promedio curso.
+El programa, al recibir como dato el promedio de un alumno en un curso
+universitario, escribe aprobado si su promedio es mayor o igual 6.
+
+PRO: variable de tipo real. */
+
+void main(void)
+{
+    float PRO;
+    printf("Ingrese el promedio del alumno: ");
+    scanf("%f", &PRO);
+    if (PRO >= 6)
+        printf("\nAprobado");
+
+
+    if (PRO < 6)
+    printf("\nReprobado");
+}
