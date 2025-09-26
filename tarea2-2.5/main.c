2f8ccfe4d5a94987dff7bd437e2f9189566ba878
Mario Mata <matamariomiguel@gmail.com>
2025-09-25 22:44:18 -0400

    tarea 2 - 5

---
+++ tarea2-2.5/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* funcion matematica.
+El programa obtiene el resultado de una funcion.
+
+OP y T: variables de tipo entero.
+RES: variable de tipo real. */
+
+void main(void)
+{
+    int OP, T;
+    float RES;
+    printf("Ingrese la opcion del calculo y el valor entero: ");
+    scanf("%d %d", &OP, &T);
+    switch(OP)
+    {
+        case 1: RES = T / 5;
+        break;
+        case 2: RES = pow(T,T);
+        /* La funcion pow esta definida en la biblioteca math.h */
+        break;
+        case 3:
+            case 4: RES = 6 * T/2;
+            break;
+
+    }
+    printf("\nResultado:  %7.2f", RES);
+}
+
