ee55545adf17b994ef4dffb0e263c3f47b25d92b
Mario Mata <matamariomiguel@gmail.com>
2025-09-30 22:17:18 -0400

    tarea2-3.16

---
+++ tarea2-3.16/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Calificaciones.
+El programa, al recibir un grupo de calificaciones de un alumno, obtiene el
+promedio de calificaciones de cada uno de ellos y, además, los alumnos con
+el mejor y peor promedio.
+
+I, MAT, MAMAT y MEMAT: variables de tipo entero.
+CAL, SUM, MAPRO, MEPRO y PRO: variables de tipo real. */
+
+int main(void)
+{
+    int I, MAT, MAMAT, MEMAT;
+    float SUM, PRO, CAL, MAPRO = 0.0, MEPRO = 11.0;
+    printf("Ingrese la matrícula del primer alumno:\t");
+    scanf("%d", &MAT);
+    while (MAT)
+    {
+        SUM = 0;
+        for (I = 1; I <= 5; I++)
+        {
+            printf("\tIngrese la calificación del alumno %d: ", I);
+            scanf("%f", &CAL);
+            SUM += CAL;
+        }
+
+        PRO = SUM / 5;
+        printf("\nMatrícula: %d\tPromedio: %5.2f", MAT, PRO);
+
+        if (PRO > MAPRO)
+        {
+            MAPRO = PRO;
+            MAMAT = MAT;
+        }
+
+        if (PRO < MEPRO)
+        {
+            MEPRO = PRO;
+            MEMAT = MAT;
+        }
+
+        printf("\n\nIngrese la matrícula del siguiente alumno (0 para terminar): ");
+        scanf("%d", &MAT);
+    }
+
+    printf("\n\nAlumno con mejor Promedio:\t%d\t%5.2f", MAMAT, MAPRO);
+    printf("\nAlumno con peor Promedio:\t%d\t%5.2f\n", MEMAT, MEPRO);
+
+    return 0;
+}
