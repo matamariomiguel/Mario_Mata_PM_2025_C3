d8db2d9fecd98f1522e5e14b60828a8f7b989f1c
Mario Mata <matamariomiguel@gmail.com>
2025-10-03 01:37:35 -0400

    tarea3-4.23

---
+++ tarea3-4.23/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void trueque(int *x, int *y)
+{
+    int tem;
+    tem = *x;
+    *x = *y;
+    *y = tem;
+}
+
+int suma(int x)
+{
+    return (x + x);
+}
